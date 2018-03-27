7d208b4f0f71579451d7d00230cee2fcb7190298
lmsv73 <BPN6AD49a>
2018-03-27 13:39:35 +0200

    Modifications CSS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,8 @@ import {Bottle, UserService} from '../api';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
