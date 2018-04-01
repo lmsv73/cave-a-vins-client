1c1bfa85d1655fcef92bc11254bfa21d7a27896f
lmsv73 <BPN6AD49a>
2018-04-01 23:12:33 +0200

    Update compartment avec verif sur le name (si existe)

--- src/app/edit-compartment/edit-compartment.component.ts
+++ src/app/edit-compartment/edit-compartment.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {EditBottleComponent} from '../edit-bottle/edit-bottle.component';
-import {CompartmentService} from '../api';
+import {Compartment, CompartmentService, UserService} from '../api';
 
 @Component({
   selector: 'app-edit-compartment',
@@ -11,13 +11,24 @@ import {CompartmentService} from '../api';
 export class EditCompartmentComponent {
 
   dataCopy: any;
+  compartments: Compartment[];
+  nameExist = false;
+  initName: string;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<EditBottleComponent>,
-    private compartmentService: CompartmentService) {
+    private compartmentService: CompartmentService,
+    private userService: UserService) {
+
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    userService.getCompartment(currentUser.user.username).subscribe(
+      data => {
+        this.compartments = data;
+      });
 
     this.dataCopy = Object.assign({}, data);
+    this.initName = this.dataCopy.name;
   }
 
   save() {
@@ -26,5 +37,14 @@ export class EditCompartmentComponent {
         this.dialogRef.close(data);
       }
     )
+  }
+
+  activeButton() {
+    this.nameExist = false;
+    for(let i = 0; i < this.compartments.length; ++i) {
+      if(this.dataCopy.name == this.compartments[i].name && this.initName != this.dataCopy.name) {
+        this.nameExist = true;
+      }
+    }
   }
 }
