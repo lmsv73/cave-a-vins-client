fcceafca881af02b01c9240c4424a39514c4f1d0
lmsv73 <BPN6AD49a>
2018-04-14 13:15:12 +0200

    Correction bug sur la récupération du token

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -7,7 +7,8 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if(currentUser) {
+
+        if(currentUser && request.headers.get("authorization") == null) {
             request = request.clone({
                 setHeaders: {
                     Authorization: `Bearer ${currentUser.token}`
